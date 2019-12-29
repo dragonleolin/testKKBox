8d97b2ae0fc4f07fe5f897ca393f1a1bbc4ac2ef
dragonleolin <wei.che0812@gmail.com>
2019-12-29 14:32:13 +0800

    Updates

---
+++ precache-manifest.6b5306e8cbecd8f2658be21b01222296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0ca277f481733725ed5caaa71e425f",
+    "url": "/dragonleolin.github.io/testKKBox/index.html"
+  },
+  {
+    "revision": "ca085bacad6ac3ead0cc",
+    "url": "/dragonleolin.github.io/testKKBox/static/css/main.aca75acb.chunk.css"
+  },
+  {
+    "revision": "065ff1b64119fc42cfa7",
+    "url": "/dragonleolin.github.io/testKKBox/static/js/2.f87b9580.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/dragonleolin.github.io/testKKBox/static/js/2.f87b9580.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca085bacad6ac3ead0cc",
+    "url": "/dragonleolin.github.io/testKKBox/static/js/main.9d83070b.chunk.js"
+  },
+  {
+    "revision": "50fa0bf1ffc1f8f7738d",
+    "url": "/dragonleolin.github.io/testKKBox/static/js/runtime-main.e68ff063.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dragonleolin.github.io/testKKBox/precache-manifest.6b5306e8cbecd8f2658be21b01222296.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dragonleolin.github.io/testKKBox/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
